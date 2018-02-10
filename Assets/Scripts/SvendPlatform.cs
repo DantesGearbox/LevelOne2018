f5867cb92098a97971197baa3adc90f2f7a95c61
Wizard <svendbreddam@gmail.com>
2018-02-10 17:52:24 +0100

    Added player tag-collision with moving platforms

--- Assets/Scripts/SvendPlatform.cs
+++ Assets/Scripts/SvendPlatform.cs
@@ -7,14 +7,16 @@ public class SvendPlatform : MonoBehaviour {
 	Rigidbody2D rb;
 
 	public float movespeed = 10.0f;
+	public bool xaxis = true;
+	public Vector2 endpoint;
+
 
 	private Transform transform;
+	private Transform scale;
+	private Vector2 startpoint;
 
 	private bool direction;
 
-	private Vector2 startpoint;
-	private Vector2 endpoint = new Vector2(0.0f, 0.0f);
-	private Vector2 position;
 
 
 	// Use this for initialization
@@ -23,25 +25,59 @@ public class SvendPlatform : MonoBehaviour {
 		startpoint = rb.position;
 		transform = rb.transform;
 
+		scale = GetComponent<Transform>();
+
 		direction = true;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(transform.position);
-		if(direction) {
-			Debug.Log("YEEEES");
-			transform.Translate(movespeed * Time.deltaTime, 0, 0);
-			if(transform.position.x > endpoint.x) {
-				direction = false;
-			}
+		//Debug.Log(transform.position);
 
-		} else {
-			Debug.Log("NOOOO");
-			transform.Translate(-movespeed * Time.deltaTime, 0, 0);
-			if(transform.position.x < startpoint.x) {
-				direction = true;
+		if(xaxis) {
+			if(direction) {
+				//Debug.Log("YEEEES");
+				transform.Translate(movespeed * Time.deltaTime, 0, 0);
+				if(transform.position.x > endpoint.x) {
+					direction = false;
+				}
+
+			} else {
+				//Debug.Log("NOOOO");
+				transform.Translate(-movespeed * Time.deltaTime, 0, 0);
+				if(transform.position.x < startpoint.x) {
+					direction = true;
+				}
 			}
+		} else {
+			if(direction) {
+				//Debug.Log("YEEEES");
+				transform.Translate(0, movespeed * Time.deltaTime, 0);
+				if(transform.position.y > endpoint.y) {
+					direction = false;
+				}
+
+			} else {
+				//Debug.Log("NOOOO");
+				transform.Translate(0, -movespeed * Time.deltaTime, 0);
+				if(transform.position.y < startpoint.y) {
+					direction = true;
+				}
+			}
+		}
+	}
+	void OnCollisionEnter2D(Collision2D other) {
+		if(other.transform.tag == "Player")
+		{
+			other.transform.parent = transform;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D other) {
+		if(other.transform.tag == "Player")
+		{
+			//Debug.Log("COLL EXIT");
+			other.transform.parent = null;
 		}
 	}
 }
